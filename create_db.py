dcb9129b9a3228b1e2d7e607116bb65957810788
yuriy.kuksarov <ashtone_91@mail.ru>
2022-02-08 22:35:32 +0300

    Создан скрипт питона создающий базу данных
    Реализация тестовая. В дальнейшем требуется:
    - данные БД вынести в конфиг файл;
    - реализовать создание таблиц

---
+++ create_db.py
@@ -0,0 +1,25 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2 import Error
+
+# conn = psycopg2.connect(dbname='postgres', user='postgres', password='1111', host='127.0.0.1')
+
+try:
+    # Подключение к серверу
+    connection = psycopg2.connect(user='postgres', password='1111', host='127.0.0.1')
+
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+    # Получение курсора
+    cursor = connection.cursor()
+
+    sql_create_database = 'create database ozon_pars'
+
+    cursor.execute(sql_create_database)
+except (Exception, Error) as error:
+    print("Error work to PostgresSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Connection close")
